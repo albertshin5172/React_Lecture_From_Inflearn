ba73324b840a70f985f46b34dada46f0470b7620
albert <alertshin5172@gmail.com>
2025-07-25 21:49:38 +0900

    chore : setting update feat : using JSX in javascript

---
+++ src/index2.js
@@ -0,0 +1,14 @@
+/* 
+lecture from inflearn 
+*/
+import ReactDOM from "react-dom";
+
+const element = (
+  <div className="container">
+    <button>Hello, world!</button>
+    <br />
+    <input value="This is a test!!!" />
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root1"));

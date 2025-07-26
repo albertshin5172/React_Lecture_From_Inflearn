722f5db997247be3388c0607a26fcb1a961bf16b
albert <alertshin5172@gmail.com>
2025-07-26 17:05:11 +0900

    feat : using conditional statement  in javascript

---
+++ src/index(2-2).js
@@ -0,0 +1,53 @@
+import ReactDOM from 'react-dom';
+
+let text = 'Hello, world!';
+const num = 15;
+const num2 = 115;
+const obj = { key: 0, a: 1, b: 2 };
+const arr = ['a', 'b', 'c', 1, 2, 3];
+const imageUrl =
+  'https://dst6jalxvbuf5.cloudfront.net/static/img/logo/logo.svg';
+
+const element = (
+  <div>
+    <h1>Input value</h1>
+    <ul>
+      <li>{text}</li>
+      <li>{text + 'test'}</li>
+    </ul>
+    <h1>Insert the numbers and the calculation fomula</h1>
+    <ul>
+      <li>{num}</li>
+      <li>{num2}</li>
+      <li>{num + num2 + 15}</li>
+    </ul>
+    <h1>Boolean, Nullish</h1>
+    <ul>
+      <li>{true}</li>
+      <li>{false}</li>
+      <li>{undefined}</li>
+      <li>{null}</li>
+    </ul>
+    <h1>Insert Object, Array</h1>
+    <ul>
+      {/* <li>{obj}</li> */}
+      <li>{arr}</li>
+      <li>{[<div>This</div>, <div>is</div>, <div>a</div>, <div>test</div>]}</li>
+    </ul>
+    <h1>Insert the Comments.</h1>
+    <ul>
+      <li>{/* This the comments.. */}</li>
+    </ul>
+    <h1>Tag</h1>
+    <ul>
+      <li>
+        <img src={imageUrl} alt="logo" />
+      </li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root2')
+);
--- src/index.js
+++ src/index.js
@@ -1,53 +1,46 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-let text = 'Hello, world!';
-const num = 15;
-const num2 = 115;
-const obj = { key: 0, a: 1, b: 2 };
-const arr = ['a', 'b', 'c', 1, 2, 3];
-const imageUrl =
-  'https://dst6jalxvbuf5.cloudfront.net/static/img/logo/logo.svg';
+const arr = [1, 2, 3];
+const text = "";
 
 const element = (
   <div>
-    <h1>Input value</h1>
+    <h1>Ternary Operator</h1>
     <ul>
-      <li>{text}</li>
-      <li>{text + 'test'}</li>
+      <li>{1 + 1 === 2 ? "TRUE" : "FALSE"}</li>
     </ul>
-    <h1>Insert the numbers and the calculation fomula</h1>
+
+    <h1>AND Operator</h1>
     <ul>
-      <li>{num}</li>
-      <li>{num2}</li>
-      <li>{num + num2 + 15}</li>
+      <li>{1 + 1 === 2 && "AND Operator"}</li>
+      <li>{arr.length && "AND Operator"}</li>
     </ul>
-    <h1>Boolean, Nullish</h1>
+
+    <h1>OR Operator</h1>
     <ul>
-      <li>{true}</li>
-      <li>{false}</li>
-      <li>{undefined}</li>
-      <li>{null}</li>
+      <li>{1 + 1 !== 2 || "OR Operator"}</li>
+      <li>{text || "OR Operator"}</li>
     </ul>
-    <h1>Insert Object, Array</h1>
-    <ul>
-      {/* <li>{obj}</li> */}
-      <li>{arr}</li>
-      <li>{[<div>This</div>, <div>is</div>, <div>a</div>, <div>test</div>]}</li>
-    </ul>
-    <h1>Insert the Comments.</h1>
-    <ul>
-      <li>{/* This the comments.. */}</li>
-    </ul>
-    <h1>Tag</h1>
+
+    <h1>IF Statement (IIFE : Immediately-invoked function expression)</h1>
     <ul>
       <li>
-        <img src={imageUrl} alt="logo" />
+        {(() => {
+          if (1 + 1 === 2) return "IF";
+          else return "ELSE";
+        })()}
+      </li>
+      <li>
+        {(() => {
+          const data = "Immediately-invoked function expression";
+
+          /* Any Operator, add available */
+          /* Typically, an IIFE preprocesses the data before executing immediately */
+          return data;
+        })()}
       </li>
     </ul>
   </div>
 );
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+ReactDOM.render(element, document.getElementById("root"));

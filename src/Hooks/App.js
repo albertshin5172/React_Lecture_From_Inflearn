d22969e32ba8468d566aa1d8b676f6f2f6af86ac
albert <alertshin5172@gmail.com>
2025-07-30 23:06:32 +0900

    feat : LifeCycle and Hooks 2 -rendering

--- src/Hooks/App.js
+++ src/Hooks/App.js
@@ -1,7 +1,24 @@
-import Counter from './Counter';
+import { useState } from 'react';
+import ClassComponent from './ClassComponent';
+import FunctionalComponent from './FunctionalComponent';
 
 function App() {
-  return <Counter />;
+  const [toggle, setToggle] = useState(true);
+
+  return (
+    <>
+      {toggle && <ClassComponent />}
+      {toggle || <FunctionalComponent />}
+
+      <hr />
+
+      <button
+        onClick={() => setToggle((t) => !t)}
+      >
+        toggle
+      </button>
+    </>
+  );
 }
 
 export default App;

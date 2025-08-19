b3d306b622e8ee39bbde42ba96bfc98b2e53b4c9
albert <alertshin5172@gmail.com>
2025-08-19 21:38:02 +0900

    feat : toy project2 - survey-pie (Section 12 / 11-2 Creating components by question type)

--- survey-pie/src/pages/CompletionPage/index.js
+++ survey-pie/src/pages/CompletionPage/index.js
@@ -1,18 +1,27 @@
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+//import './index.css';
+
+const a = 123;
 
 function CompletionPage() {
-  return (
-    <>
-      <div>CompletionPage</div>
-      <div>
-        {/* Using this method will cause the entire page to refresh, which is not recommended. */}
-        <a href="/">move to SurveyPage by a tag</a>
-        <br />
-        {/* Using this method will cause React Router to handle it internally (recommended). */}
-        <Link to="/">move to SurveyPage</Link>
-      </div>
-    </>
-  );
+  //return <div className="CompletionPage">CompletionPage</div>;
+  return <CompletionPageWrapper>CompletionPage</CompletionPageWrapper>;
+  // return (
+  //   <>
+  //     <div>CompletionPage</div>
+  //     <div>
+  //       {/* Using this method will cause the entire page to refresh, which is not recommended. */}
+  //       <a href="/">move to SurveyPage by a tag</a>
+  //       <br />
+  //       {/* Using this method will cause React Router to handle it internally (recommended). */}
+  //       <Link to="/">move to SurveyPage</Link>
+  //     </div>
+  //   </>
+  // );
 }
-
+const CompletionPageWrapper = styled.div`
+  background: aqua;
+  padding: ${a === 123 ? '4em' : '1em'};
+`;
 export default CompletionPage;
